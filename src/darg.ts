6c2060d451b16a188c298e4e1c38f504c2a6929b
Kevin Gimbel <hallo@kevingimbel.com>
2020-05-24 11:44:32 +0200

    feat: Allow help if required arg is missing; Restructure

--- src/darg.ts
+++ src/darg.ts
@@ -1,5 +1,4 @@
-import { DargArgumentValue, DargConfig } from "./types.ts";
-import { _darg_get_deno_arg } from "./args.ts";
+import { DargArgumentValue } from "./types.ts";
 
 export class Darg {
   private value: DargArgumentValue;
@@ -7,7 +6,7 @@ export class Darg {
   constructor() {}
 
   parse(arg: string, is_required?: boolean): Darg {
-    let v = _darg_get_deno_arg(arg, is_required);
+    let v = this.get_deno_arg(arg, is_required);
     this.value = v;
     return this;
   }
@@ -23,5 +22,37 @@ export class Darg {
   is_help(): boolean {
     return (Deno.args.indexOf("-h") >= 0 || Deno.args.indexOf("--help") >= 0 ||
       Deno.args.indexOf("help") >= 0);
+  }
+
+  private get_deno_arg(
+    arg: string,
+    required?: boolean,
+  ): string {
+    let wanted: any[] = [arg, `-${arg[0]}`, `--${arg}`];
+    let argument_value = "";
+
+    for (let a in wanted) {
+      let index = Deno.args.indexOf(wanted[a]);
+      if (index >= 0) {
+        argument_value = Deno.args[index + 1];
+        break;
+      }
+    }
+
+    if (required && argument_value == "") {
+      // Don't log and exit if help was requested
+      if (!this.is_help()) {
+        this.log_arg_required(arg);
+        Deno.exit(1);
+      }
+    }
+
+    return argument_value;
+  }
+
+  private log_arg_required(arg: string) {
+    console.error(
+      `Error: Argument ${arg} (or -${arg[0]}, --${arg}) is required`,
+    );
   }
 }
